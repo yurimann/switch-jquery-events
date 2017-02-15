6b2c686ebf173b452f751e9205a08e236446c200
Yuri <yuri.manahan@gmail.com>
2017-02-15 15:38:06 -0500

    Switch turns on and off

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 
+$(document).ready(function(){
+  $('h1').text('Click me!')
 
+  var click = $('.switch button')
+  click.on('click', function(){
+    if (click.attr('class') === 'on'){
+      click.attr('class', 'off')
+      $('body').attr('class', 'dark');
+    }
+    else {
+      click.attr('class', 'on')
+      $('body').attr('class', 'light');
+    }
+  });
+
+});
